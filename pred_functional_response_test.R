98c5af22eba0c8c29c5938f5d948ffe229d7365b
Jonas Kemp <jonasbkemp@gmail.com>
2016-06-27 23:04:08 -0700

    Edits to functional response (still in progress - need to resolve scale issue)

--- pred_functional_response_test.R
+++ pred_functional_response_test.R
@@ -1,58 +1,73 @@
-predation<-function(P, P.L, N1, N2, N3){
+## Modeling functional response of prawn on different size classes of snails
+
+## To-do list:
+  # Refit attack rate and handling time functions (EDIT: see new ones below)
+  # Resolve scale issue
+  # Get the shape of the curve right in single-class and mixed-class populations
+  # Integrate into dynamic model
+
+predation = function(P, P.L, N1, N2, N3) {
   
   N = N1 + N2 + N3
   
-  P.bm = (0.096868*(P.L/10)^3.2944)/10 #prawn length (mm) to weight (g) conversion from Lalrisanga et al (rosenbergii)
+  P.bm = (0.096868*(P.L/10)^3.2944)/10 # Prawn length (mm) to weight (g) conversion,
+                                       # based on Lalrisanga et al. 2012 (for M. rosenbergii)
   
-  N1.bm = 0.3124*0.4^2 - 0.1205*0.4 #4mm size class; length to weight from Sanna's data
-  N2.bm = 0.3124*0.8^2 - 0.1205*0.8 #8mm size class; length to weight from Sanna's data
-  N3.bm = 0.3124*1.2^2 - 0.1205*1.2 #12mm size class; length to weight from Sanna's data
+  N1.bm = 0.3124*0.4^2 - 0.1205*0.4 # 4mm size class; length-to-weight conversion based on Sanna's data on B. glabrata
+  N2.bm = 0.3124*0.8^2 - 0.1205*0.8 # 8mm size class
+  N3.bm = 0.3124*1.2^2 - 0.1205*1.2 # 12mm size class
   
+  # Ratios of prawn-to-snail biomass
   rps1 = P.bm / N1.bm
   rps2 = P.bm / N2.bm
   rps3 = P.bm / N3.bm
   
-  ## TODO: refit functions for alpha and Th
-  alpha1 = 1+0.03*rps1 #attack rate as a function of prawn to snail biomass ratio
-  alpha2 = 1+0.03*rps2 #attack rate as a function of prawn to snail biomass ratio
-  alpha3 = 1+0.03*rps3 #attack rate as a function of prawn to snail biomass ratio
+  # Attack rates as a function of biomass ratio
+  alpha1 = 0.037192*rps1 # Prev. function: 1+0.03*rps
+  alpha2 = 0.037192*rps2
+  alpha3 = 0.037192*rps3
   
-  handle1 = (0.53623*exp(-0.05167*rps1))^-1 #handling time as a function of prawn to snail mass ratio
-  handle2 = (0.53623*exp(-0.05167*rps2))^-1 #handling time as a function of prawn to snail mass ratio
-  handle3 = (0.53623*exp(-0.05167*rps3))^-1 #handling time as a function of prawn to snail mass ratio
+  # Handling times as a function of biomass ratio
+  handle1 = 1/(0.40450*rps1) # Prev. function: 0.53623*exp(-0.05167*rps)
+  handle2 = 1/(0.40450*rps2)
+  handle3 = 1/(0.40450*rps3)
   
+  # Preference variables, currently ignoring these
   gam1 = 1 #1/3 + 0.5*exp(-0.15*P.bm)
   gam3 = 1 #1/3*exp(-12*exp(-0.2*P.bm))
   gam2 = 1 #1 - gam1 - gam3
   
-  ## Probably needs tweaking
-  #gam_star1 = gam1*(N1/N) / (gam1*(N1/N) + gam2*(N2/N) + gam3*(N3/N))
-  #gam_star2 = gam2*(N2/N) / (gam1*(N1/N) + gam2*(N2/N) + gam3*(N3/N))
-  #gam_star3 = gam3*(N3/N) / (gam1*(N1/N) + gam2*(N2/N) + gam3*(N3/N))
+  # Adjusted preference variables, currently ignoring these
+  # gam_star1 = gam1*(N1/N) / (gam1*(N1/N) + gam2*(N2/N) + gam3*(N3/N))
+  # gam_star2 = gam2*(N2/N) / (gam1*(N1/N) + gam2*(N2/N) + gam3*(N3/N))
+  # gam_star3 = gam3*(N3/N) / (gam1*(N1/N) + gam2*(N2/N) + gam3*(N3/N))
   
-  psi1 = (P*alpha1*gam1*N1) / (1+sum((alpha1/handle1)*gam1*N1,
-                                     (alpha2/handle2)*gam2*N2,
-                                     (alpha3/handle3)*gam3*N3))
+  # Size-class specific functional responses
+  psi1 = (P*alpha1*gam1*N1) / (1+sum(alpha1*handle1*gam1*N1,
+                                     alpha2*handle2*gam2*N2,
+                                     alpha3*handle3*gam3*N3))
   
-  psi2 = (P*alpha2*gam2*N2) / (1+sum((alpha1/handle1)*gam1*N1,
-                                     (alpha2/handle2)*gam2*N2,
-                                     (alpha3/handle3)*gam3*N3))
+  psi2 = (P*alpha2*gam2*N2) / (1+sum(alpha1*handle1*gam1*N1,
+                                     alpha2*handle2*gam2*N2,
+                                     alpha3*handle3*gam3*N3))
   
-  psi3 = (P*alpha3*gam3*N3) / (1+sum((alpha1/handle1)*gam1*N1,
-                                     (alpha2/handle2)*gam2*N2,
-                                     (alpha3/handle3)*gam3*N3))
+  psi3 = (P*alpha3*gam3*N3) / (1+sum(alpha1*handle1*gam1*N1,
+                                     alpha2*handle2*gam2*N2,
+                                     alpha3*handle3*gam3*N3))
   
+  # Aggregate functional response
   psi = psi1+psi2+psi3
   
-  return(psi)
+  return(c(psi1,psi2,psi3,psi))
 }
 
-## TODO: experiment with curves for different prawn sizes and snail populations (single or mixed size)
-fr = rep.int(0, 51)
-for (i in 0:150) {
-  fr[i+1] = predation(P = 1, P.L = 75, N1 = i, N2 = 0, N3 = 0)
+# Plot functional response for a given prawn size and snail population
+x = c(0:50)
+fr = numeric(length = length(x))
+for (i in x) {
+  fr[i+1] = predation(P = 1, P.L = 50, N1 = i, N2 = 0, N3 = 0)[4]
 }
-plot(c(0:150), fr, type="l")
+plot(x, fr, type="l", xlab = "Snail density", ylab = "Functional response")
 
 
 
