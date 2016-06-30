b56009e79e10c7febddfc631bd8dedf30afccb5f
Jonas Kemp <jonasbkemp@gmail.com>
2016-06-29 23:35:28 -0700

    Added area scaling to size class predation model, tweaked to match expected behavior

--- snail_size_classes_predation.R
+++ snail_size_classes_predation.R
@@ -1,9 +1,9 @@
 ## Modeling dynamics of prawn predation on snails; no disease
 
 ## To-do list:
-  # Scale attack rate, density-dependent parameters appropriately by area
-  # Vary prawn size, eventually include growth dynamics
-  # Look for snail elimination thresholds (plot N vs. P)
+  # Integrate prawn growth dynamics
+  # Note: attack rate scale factor changed from 1/10 in homogeneous model to 1/50 in size class model 
+  #   to match expected elimination threshold; may require further exploration/tweaking
 
 require(deSolve)
 
@@ -30,6 +30,11 @@ snail_size_predation = function(t, n, parameters) {
     alpha2 = 0.037192*rps2
     alpha3 = 0.037192*rps3
     
+    # Adjusted attack rates, accounting for area of interest and limiting factors in the wild
+    alpha_star1 = alpha1*s/A
+    alpha_star2 = alpha2*s/A
+    alpha_star3 = alpha3*s/A
+    
     # Handling times as a function of biomass ratio, fit from Sokolow et al. 2014
     handle1 = 1/(0.40450*rps1)
     handle2 = 1/(0.40450*rps2)
@@ -41,15 +46,15 @@ snail_size_predation = function(t, n, parameters) {
     # dLdt = k/(1+gam*P.bmt)*(linf - L) # Mean prawn length, with growth rate k, max length linf, crowding parameter gam
     
     # Functional response by snail size class
-    psi1 = (P*alpha1) / (1+sum(alpha1*handle1*S1, alpha2*handle2*S2, alpha3*handle3*S3))
-    psi2 = (P*alpha2) / (1+sum(alpha1*handle1*S1, alpha2*handle2*S2, alpha3*handle3*S3))
-    psi3 = (P*alpha3) / (1+sum(alpha1*handle1*S1, alpha2*handle2*S2, alpha3*handle3*S3))
+    psi1 = (P*alpha_star1) / (1+sum(alpha_star1*handle1*S1, alpha_star2*handle2*S2, alpha_star3*handle3*S3))
+    psi2 = (P*alpha_star2) / (1+sum(alpha_star1*handle1*S1, alpha_star2*handle2*S2, alpha_star3*handle3*S3))
+    psi3 = (P*alpha_star3) / (1+sum(alpha_star1*handle1*S1, alpha_star2*handle2*S2, alpha_star3*handle3*S3))
     
     # Total snail population  
     N = S1+S2+S3
     
     # Snail dynamics
-    dS1dt = f*(1-N/Kn)*(S2+S3) - muN1*S1 - psi1*S1 - g1*S1
+    dS1dt = f*(1-N/(Kn*A))*(S2+S3) - muN1*S1 - psi1*S1 - g1*S1
     
     dS2dt = g1*S1 - muN2*S2 - psi2*S2 - g2*S2
     
@@ -61,13 +66,13 @@ snail_size_predation = function(t, n, parameters) {
 } 
 
 # Set initial values and parameters
-nstart = c(S1 = 5000, S2 = 2000, S3 = 1000)
+nstart = c(S1 = 4000, S2 = 2000, S3 = 2000)
 time = seq(0, 365*2, 1)
 
 parameters=c(
   # Prawn model parameters
   P = 50,        # Number of prawns, currently held constant
-  L = 75,        # Length of prawns (mm)
+  L = 95,        # Length of prawns (mm)
   ap = 0.096868, # Allometric coefficient based on Lalrisanga et al. 2012 (for M. rosenbergii)
   bp = 3.2944,   # Allometric coefficient based on Lalrisanga et al. 2012 (for M. rosenbergii)
   # gam = 1e-6, # Crowding parameter, reduces growth rate at high density
@@ -85,17 +90,31 @@ parameters=c(
   muN3 = 1/60,      # Natural mortality rate of large snails (deaths/snail/day; assume mean lifespan = 50 days)
   g1 = 1/37,        # Growth rate of small snails (size class transition rate, in terms of days to grow 4mm; adapted from McCreesh et al. 2014, assuming water temp. of 25 C) 
   g2 = 1/62,        # Growth rate of medium snails (size class transition rate, in terms of days to grow 4mm; adapted from McCreesh et al. 2014, assuming water temp. of 25 C) 
-  Kn = 10000        # Carrying capacity of snails (per 200m^2 site, Sokolow et al. 2015) 
+  Kn = 50,          # Carrying capacity of snails (per m^2, Sokolow et al. 2015)
+  A = 200,          # Area of site of interest, m^2
+  s = 1/50          # Scale factor limiting prawn attack rate in the wild (vs. lab conditions);
+                    #   decreased by factor of 5 in size class model to match expected elimination threshold
 )
 
 
 # Run & plot
 output_ss = as.data.frame(ode(nstart,time,snail_size_predation,parameters))
 plot(output_ss$time, output_ss$S1, type = 'l', col = 'red', lwd=2, xlab = "Time (days)", ylab = "Snail pop.",
-     ylim = c(0, 5000))
+     ylim = c(0, 10000), main = paste('P = ', parameters['P'], ',  L = ', parameters['L'], sep = ''))
 lines(output_ss$time, output_ss$S2, col = 'green', lwd=2)
 lines(output_ss$time, output_ss$S3, col = 'blue', lwd=2)
-legend("topright", c("S","M","L"), col = c("red","green","blue"), lty = 1, lwd = 2)
+lines(output_ss$time, output_ss$S1+output_ss$S2+output_ss$S3, col = 'black', lwd=2)
+legend("topright", c("S","M","L","T"), col = c("red","green","blue","black"), lty = 1, lwd = 2)
 
+# Plot equilibrium snail abundance vs. prawn density
+x = c(0:100)
+s.pop = numeric(length(x))
+for (i in x) {
+  parameters["P"] = i
+  output_ss = as.data.frame(ode(nstart,time,snail_size_predation,parameters))
+  s.pop[i+1] = tail(output_ss$S1, n = 1)+tail(output_ss$S2, n = 1)+tail(output_ss$S3, n = 1)
+}
+plot(x, s.pop, type = 'l', col = 'blue', lwd=2, xlab = "Number of prawns", ylab = "Equilibrium snail pop.",
+     ylim = c(0, 10000), main = paste('L = ', parameters['L'], sep = ''))
 
 
