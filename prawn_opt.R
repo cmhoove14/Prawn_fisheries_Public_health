4bb8617bb2ad063fb33ca628c93c6ae58c6314ec
Jonas Kemp <jonasbkemp@gmail.com>
2016-07-11 16:29:13 -0700

    Added plot of estimated profit and optimal stocking density; added alternative growth rate for M. rosenbergii

--- prawn_opt.R
+++ prawn_opt.R
@@ -2,10 +2,8 @@
 
 ## To-do list:
   # Fit density-dependent parameters to data from Ranjeet & Kurup 2010 (or other Macrobrachium fisheries data)
-    # Note: update distributions used for fitting - survival shouldn't be normal (use beta distribution?)
-  # Look into other parameter estimates: M. rosenbergii vs. M. vollenhovenii, extensive vs. intensive aquaculture
-  # Try to optimize profit based on stocking density of post-larvae
-    # Objective f'n: profit = p*H(SD)*e^(-d*T(SD)) - c*SD
+    # Note: may also want to use net production data for fitting?
+  # Investigate price-to-cost ratios in other locations for sensitivity analysis on maximum profit estimation
 
 require(deSolve)
 
@@ -28,18 +26,19 @@ prawn_biomass = function(t, n, parameters) {
 } 
 
 # Set initial values and parameters
-nstart = c(P = 15000, L = 25)
+# Stocking conditions: 19,000/ha maximizes profit in current model; L = 25mm ~ W = 0.2g
+nstart = c(P = 19000, L = 25)
 time = seq(0, 365*2, 1)
 
 parameters=c(
-  a = 0.096868,     # Allometric parameter for prawn length-weight relationship, from Lalrinsanga et al. 2012 (growout phase)
-  b = 3.2944,       # Allometric parameter for prawn length-weight relationship, from Lalrinsanga et al. 2012 (growout phase)
-  gam = 1e-6,       # Density-dependent growth parameter (based on biomass per hectare); NEEDS TO BE FIT
-  mu = 0.006136986, # Natural prawn mortality rate, from Nwosu & Wolfi 2006
+  a = 0.096868,     # Allometric parameter for prawn length-weight relationship, from Lalrinsanga et al. 2012 (M. rosenbergii, growout phase)
+  b = 3.2944,       # Allometric parameter for prawn length-weight relationship, from Lalrinsanga et al. 2012 (M. rosenbergii, growout phase)
+  gam = 1e-6,       # Density-dependent growth parameter (based on biomass per hectare); not yet fit
+  mu = 0.006136986, # Natural prawn mortality rate, from Nwosu & Wolfi 2006 (M. vollenhovenii)
   d = -0.25,        # Exponential relationship of size with mortality; no source
-  phi = 40000000,   # Density-dependent mortality parameter (based on biomass per hectare); NEEDS TO BE FIT
-  k = 0.00339726,   # Growth rate (mm/day), from Nwosu & Wolfi 2006
-  linf = 206        # Max length (mm), from Nwosu & Wolfi 2006
+  phi = 40000000,   # Density-dependent mortality parameter (based on biomass per hectare); not yet fit
+  k = 0.00339726,   # Growth rate (mm/day), from Nwosu & Wolfi 2006 (M. vollenhovenii); alternate value for M. rosenbergii, from Sampaio & Wagner 1996: 0.0104333333
+  linf = 206        # Max length (mm), from Nwosu & Wolfi 2006 (M. vollenhovenii)
 )
 
 
@@ -68,7 +67,7 @@ plot(x = output$time, y = output$P/100, col = 'red', xlab = 'Time (days)', ylab 
                            paste('Time of harvest =', round(harvest.time), 'days', sep = ' ')), cex=0.5)
   
 # Plot amount of harvest, biomass multiplier, optimal harvest time against initial stocking density
-x = c(0:50)
+x = c(1:50)
 harvest = numeric(length(x))
 harvest.m = numeric(length(x))
 harvest.t = numeric(length(x))
@@ -77,68 +76,82 @@ for (i in x) {
   output.d = as.data.frame(ode(nstart.d,time,prawn_biomass,parameters))
   output.d$B = ((parameters['a']*(output.d$L/10)^parameters['b'])/10)
   output.d$Bt = output.d$B*output.d$P
-  harvest[i+1] = max(output.d$Bt)/1000
-  harvest.m[i+1] = max(output.d$Bt)/output.d$Bt[output.d$time==1]
-  harvest.t[i+1] = output.d$time[output.d$Bt == max(output.d$Bt)]
+  harvest[i] = max(output.d$Bt)/1000
+  harvest.m[i] = max(output.d$Bt)/output.d$Bt[output.d$time==1]
+  harvest.t[i] = output.d$time[output.d$Bt == max(output.d$Bt)]
 }
 plot(x, harvest, col = 'blue', xlab = 'Stocking density (thousand prawns/ha)', ylab = 'Harvest (kg/ha)',
      type = 'l', lwd = 2, main = 'Harvest by initial stocking density')
-plot(x[-1], harvest.m[-1], col = 'blue', xlab = 'Stocking density (thousand prawns/ha)', ylab = 'Biomass multiplier', 
+plot(x, harvest.m, col = 'blue', xlab = 'Stocking density (thousand prawns/ha)', ylab = 'Biomass multiplier', 
      type = 'l', lwd = 2, main = 'Biomass multiplier by stocking density')
-plot(x[-1], harvest.t[-1], col = 'blue', xlab = 'Stocking density (thousand prawns/ha)', ylab = 'Harvest time (days)',
+plot(x, harvest.t, col = 'blue', xlab = 'Stocking density (thousand prawns/ha)', ylab = 'Harvest time (days)',
      type = 'l', lwd = 2, main = 'Harvest time by stocking density')
 
 
+# Profit optimization (price estimates from Tamil Nadu Agricultural University, http://agritech.tnau.ac.in/fishery/fish_freshwaterprawn.html)
+p = 140                                           # Weighted average market price of prawns, in rupees/kg 
+c = 600                                           # Cost of post-larvae, in rupees/1000 PL
+delta = -log(1-0.1)/365                           # Discount rate, equivalent to 10%/year
+profit = p*harvest*exp(-delta*(harvest.t)) - c*x  # Profit function, in terms of revenue (discounted by time to harvest) minus stocking costs 
+plot(x, profit, col = 'green', xlab = 'Stocking density (thousand prawns/ha)', ylab = 'Profit (rupees/ha)',
+     type = 'l', lwd = 2, main = 'Estimated profit by stocking density')
+  abline(v = which.max(profit), lty = 2, lwd = 2)
+
+
 # Fit density-dependent parameters to data from Ranjeet & Kurup 2010
-prawn.mle = function(gam, phi) {
-  parameters['gam'] = gam
-  parameters['phi'] = phi
+beta.params = function(mu, var) {
+  alpha = ((1 - mu)/var - 1/mu)*mu^2
+  beta = alpha*(1/mu - 1)
+  return(list(alpha = alpha, beta = beta))
+}
+
+prawn.mle = function(params) {
+  parameters['gam'] = params[1]
+  parameters['phi'] = params[2]
   
   time = seq(0,30*8,1)
   
   nstart1 = c(P = 5000, L = 25)
-  output1 = as.data.frame(ode(nstart1,time,prawn_biomass,parameters))
+  output1 = as.data.frame(ode(nstart1, time, prawn_biomass, parameters))
   mean.size1 = ((parameters['a']*(output1$L[output1$time == max(time)]/10)^parameters['b'])/10)
-  surv1 = output1$P[output1$time == 0] / output1$P[output1$time == max(time)]
+  surv1 = output1$P[output1$time == max(time)] / output1$P[output1$time == 0]
     
   nstart2 = c(P = 10000, L = 25)
-  output2=as.data.frame(ode(nstart2,time,prawn_biomass,parameters))
+  output2 = as.data.frame(ode(nstart2, time, prawn_biomass, parameters))
   mean.size2 = ((parameters['a']*(output2$L[output2$time == max(time)]/10)^parameters['b'])/10)
-  surv2 = output2$P[output2$time == 0] / output2$P[output2$time == max(time)]
+  surv2 = output2$P[output2$time == max(time)] / output2$P[output2$time == 0]
     
   nstart3 = c(P = 15000, L = 25)
-  output3=as.data.frame(ode(nstart3,time,prawn_biomass,parameters))
+  output3 = as.data.frame(ode(nstart3, time, prawn_biomass, parameters))
   mean.size3 = ((parameters['a']*(output3$L[output3$time == max(time)]/10)^parameters['b'])/10)
-  surv3 = output3$P[output3$time == 0] / output3$P[output3$time == max(time)]
+  surv3 = output3$P[output3$time == max(time)] / output3$P[output3$time == 0]
     
   nstart4 = c(P = 25000, L = 25)
-  output4=as.data.frame(ode(nstart4,time,prawn_biomass,parameters))
+  output4 = as.data.frame(ode(nstart4, time, prawn_biomass, parameters))
   mean.size4 = ((parameters['a']*(output4$L[output4$time == max(time)]/10)^parameters['b'])/10)
-  surv4 = output4$P[output4$time == 0] / output4$P[output4$time == max(time)]
+  surv4 = output4$P[output4$time == max(time)] / output4$P[output4$time == 0]
   
-  ll1 = dnorm(mean.size1, mean = 101.650, sd = 7.5067)
-  ll2 = dnorm(surv1, mean = 0.69440, sd = 0.066525)
-  ll3 = dnorm(mean.size2, mean = 87.730, sd = 8.1917)
-  ll4 = dnorm(surv2, mean = 0.54533, sd = 0.038724)
-  ll5 = dnorm(mean.size3, mean = 69.113, sd = 8.6865)
-  ll6 = dnorm(surv3, mean = 0.4269, sd = 0.063707)
-  ll7 = dnorm(mean.size4, mean = 55.483, sd = 3.9793)
-  ll8 = dnorm(surv4, mean = 0.28210, sd = 0.021198)
-
-  LL.fin = ll1*ll2*ll3*ll4*ll5*ll6*ll7*ll8
-  negLL = -log(LL.fin+1)
+  lh.sz1 = dnorm(mean.size1, mean = 101.650, sd = 7.5067)
+  lh.sz2 = dnorm(mean.size2, mean = 87.730, sd = 8.1917)
+  lh.sz3 = dnorm(mean.size3, mean = 69.113, sd = 8.6865)
+  lh.sz4 = dnorm(mean.size4, mean = 55.483, sd = 3.9793)
+  
+  mu1 = 0.69440; var1 = 0.066525^2
+  lh.sv1 = dbeta(surv1, shape1 = beta.params(mu1, var1)$alpha, shape2 = beta.params(mu1, var1)$beta)
+  mu2 = 0.54533; var2 = 0.038724^2
+  lh.sv2 = dbeta(surv2, shape1 = beta.params(mu2, var2)$alpha, shape2 = beta.params(mu2, var2)$beta)
+  mu3 = 0.4269; var3 = 0.063707^2
+  lh.sv3 = dbeta(surv3, shape1 = beta.params(mu3, var3)$alpha, shape2 = beta.params(mu3, var3)$beta)
+  mu4 = 0.28210; var4 = 0.021198^2
+  lh.sv4 = dbeta(surv4, shape1 = beta.params(mu4, var4)$alpha, shape2 = beta.params(mu4, var4)$beta)
+  
+  negLL = -(log(lh.sz1)+log(lh.sz2)+log(lh.sz3)+log(lh.sz4)+log(lh.sv1)+log(lh.sv2)+log(lh.sv3)+log(lh.sv4))
   return(negLL)
 }
 
-prawn.optim(params = c(1e-7, 3e8))
+prawn.mle(c(1e-6, 4e8))
 
-op.df = data.frame('gams' = seq(1e-7, 1e-5, 1e-7),
-                   'phis' = seq(1e7, 1e9, 1e7),
-                   'negLL' = 0) 
-for(i in 1:nrow(op.df)){
-  params = c(op.df[i,1], op.df[i,2])
-  op.df[i,3] = prawn.optim(params)
-}
+## DOESN'T WORK - ODE solver runs into problems during optimization
+density.params.fit = optim(par=c(1e-6, 4e8), prawn.mle, method = 'Nelder-Mead')
 
-op.bm<-optim(par=c(1e-6, 4e8), prawn.optim, method = 'Nelder-Mead', control = list(fnscale = -1))
 
