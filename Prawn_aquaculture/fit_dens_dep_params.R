59794abf46c37f960630d3af1c6c7a1a501dc665
Chris Hoover <cmhoove14@gmail.com>
2018-01-20 18:24:48 -0800

    Code to informally "fit" density dependent parameters of the prawn aquaculture model to trials conducted by ranjeet and Kurup

---
+++ Prawn_aquaculture/fit_dens_dep_params.R
@@ -0,0 +1,50 @@
+source('Prawn_aquaculture/macrobrachium_aquaculture_data.R')
+source('Prawn_aquaculture/prawn_aquaculture_mod.R')
+
+t.rk06 = c(1:(8*30)) # time to harvest in Ranjeet and Kurup trials is 8 months 
+
+nsim = 20
+surv.df = expand.grid(om = seq(5e-10, 1e-8, length.out = nsim), 
+                      gam = seq(1e-6, 1e-5, length.out = nsim),
+                      P0 = seq(10000, 70000, length.out = nsim))
+
+surv.df$surv = 0
+surv.df$mean_B_perday = 0
+
+par.aqua['k'] = 0.0104333333       # Growth rate (mm/day, M. rosenbergii)
+
+for(k in 1:nrow(surv.df)){
+  start = c(P = surv.df[k,3], L = 38)  # set starting density
+  par.aqua['gam'] = surv.df[k,2]       # set gamma parameter
+  par.aqua['om'] = surv.df[k,1]        # set omega parameter
+  
+  op = as.data.frame(ode(start,t.rk06,prawn_biomass,par.aqua))   # run simulation
+  op$B = ((par.aqua['a.p']*(op$L/10)^par.aqua['b.p'])/10)        # Mean prawn biomass, transformed from length
+  
+  surv.df[k,4] = op$P[op$time == max(op$time)]/surv.df[k,3]      # store survival rate in simulation
+  surv.df[k,5] = op$B[op$time == max(op$time)]/max(op$time)      # store mean daily growth rate in simulation  
+  
+  if(k %% 1000==0) print(k)
+}
+
+for(o in 1:nsim){
+  plot(surv.df$P0[surv.df$om == unique(surv.df$om)[o]]/10000, surv.df$surv[surv.df$om == unique(surv.df$om)[o]],
+       pch = 16, ylim = c(0,1), xlab = 'Stocking density (P/m^2', ylab = '%survival',
+       main = paste0('omega = ', unique(surv.df$om)[o]))
+    abline(om.lm, lty = 2, col = 2)
+}
+
+om.gg
+
+# 5e-9 fits best
+
+for(g in 1:nsim){
+  plot(surv.df$P0[surv.df$gam == unique(surv.df$gam)[g]]/10000, surv.df$mean_B_perday[surv.df$gam == unique(surv.df$gam)[g]],
+       pch = 16, ylim = c(0,1), xlab = 'Stocking density (P/m^2', ylab = 'Mean size',
+       main = paste0('gamma = ', unique(surv.df$gam)[g]))
+    abline(gam.lm, lty = 2, col = 2)
+}
+
+gam.gg
+
+# 6e-6 fits best
