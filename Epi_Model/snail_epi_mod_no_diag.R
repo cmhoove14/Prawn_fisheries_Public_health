acd633766dadadba04de5b92156b38d43ad45910
Chris Hoover <cmhoove14@gmail.com>
2018-07-25 17:06:45 -0700

    Tweaks to model with no diagonal transitions, new script for plotting simulations

--- Epi_Model/snail_epi_mod_no_diag.R
+++ Epi_Model/snail_epi_mod_no_diag.R
@@ -10,6 +10,7 @@
   # Come up with R0 expression?
 
 require(deSolve)
+require(tidyverse)
 
 #mating function
 fx<-function(x, mean.worm, clump){
@@ -21,7 +22,58 @@ phi_Wk<-function(W, phi){
   1-( (1-alpha)^(phi+1) * (integrate(fx, 0, 2*pi, W, phi, stop.on.error = F)$value) /(2*pi)  )
 }
 
-snail_epi = function(t, n, parameters) { 
+# Set initial values and parameters #######
+area = 1000 #1 square kiolmeter = 1000m^2
+nstart.sn = c(S1 = 5*area, S2 = 0, S3 = 0, 
+              E1 = 0, E2 = 0, E3 = 0, 
+              I1 = 0, I2 = 0, I3 = 0, W = 10)
+t.sn = seq(0,365*300,10)
+
+par.snails=c(
+  ## Location parameters
+  A = area,          # Area of site of interest, m^2
+  H = 1.5 * area,          # Human population at site of interest
+  
+  ## Reproductive parameters
+  f = 0.26,          # Birth rate of adult snails (snails/reproductive snail/day, including survival to detection; more like a recruitment rate)
+  Kn = 50*area,      # Carrying capacity of snails (snails/m^2), from Sokolow et al. 2015
+  z = 0.5,           # Fraction of exposed snails that can reproduce, from Sokolow et al. 2015
+  
+  ## Snail mortality parameters
+  muN1 = 1/50,       # Natural mortality rate of small snails (deaths/snail/day; assume mean lifespan = 50 days)
+  muN2 = 1/75,       # Natural mortality rate of medium snails (deaths/snail/day; assume mean lifespan = 75 days)
+  muN3 = 1/100,      # Natural mortality rate of large snails (deaths/snail/day; assume mean lifespan = 100 days)
+  muI = 1/10,        # Mortality rate of infected snails
+
+  ## Predation parameters
+  psi1 = 0,          # Predation rate of prawns on small snails, ignored in this model
+  psi2 = 0,          # Predation rate of prawns on medium snails, ignored in this model
+  psi3 = 0,          # Predation rate of prawns on large snails, ignored in this model
+  
+  ## Snail growth parameters
+  g1 = 1/37,         # Growth rate of small snails (size class transition rate, in terms of days to grow 4mm; adapted from McCreesh et al. 2014, assuming water temp. of 25 C)
+  g2 = 1/62,         # Growth rate of medium snails (size class transition rate, in terms of days to grow 4mm; adapted from McCreesh et al. 2014, assuming water temp. of 25 C)
+  
+  ## Infection parameters
+  beta = 4e-6,       # Human-to-snail infection probability in reference area (infected snails/miracidia/snail/day); from Sokolow et al. 2015 
+  m = 0.8,           # Miracidial shedding rate per adult female worm divided by miracidial mortality; from Sokolow et al. 2015
+  sigma = 1/50,      # Latent period for exposed snails (infectious snails/exposed snail/day); from Sokolow et al. 2015 
+  lambda = 0.005,  # Snail-to-human infection probability scaled to 1 m^2 (composite including cercarial shedding, mortality, infection, survival to patency) from Sokolow et al. 2015
+  theta1 = 167.8/127.8,         # Scale factor describing increase in cercarial shedding rate in larger (size class 2) snails; from Chu & Dawood 1970 (estimated to be between 2 and 10)
+  theta2 = 1006.8/127.8,         # Scale factor describing increase in cercarial shedding rate in larger (size class 2) snails; from Chu & Dawood 1970 (estimated to be between 2 and 10)
+
+  phi = 0.2,           # clumping parameter of the negative binomial distribution used in the mating probability function
+  
+  ## Schisto mortality parameters
+  muW = 1/(3.3*365), # Natural mortality rate of adult worms in humans, assuming average lifespan of 3.3 years, from Sokolow et al. 2015
+  muH = 1/(60*365)   # Natural mortality of humans (contributing to worm mortality), assuming average lifespan of 60 years, from Sokolow et al. 2015
+)
+
+
+
+
+#Model with no diagonal transitions, all horizontal and vertical transitions #####
+snail_epi_allvh = function(t, n, parameters) { 
   with(as.list(parameters),{
     
     S1=n[1]
@@ -49,70 +101,25 @@ snail_epi = function(t, n, parameters) {
     
     dS3dt = g2*S2 - muN3*S3 - psi3*S3 - (beta)*M*H*S3
     
-    dE1dt = (beta)*M*H*S1 - muN1*E1 - psi1*E1 - sigma*E1
+    dE1dt = (beta)*M*H*S1 - muN1*E1 - psi1*E1 - sigma*E1 - g1*E1
     
-    dE2dt = (beta)*M*H*S2 - muN2*E2 - psi2*E2 - sigma*E2
+    dE2dt = (beta)*M*H*S2 + g1*E1 - muN2*E2 - psi2*E2 - sigma*E2 - g2*E2
     
-    dE3dt = (beta)*M*H*S3 - muN3*E3 - psi3*E3 - sigma*E3
+    dE3dt = (beta)*M*H*S3 + g2*E2 - muN3*E3 - psi3*E3 - sigma*E3
     
-    dI1dt = sigma*E1 - (muN1*3)*I1 - psi2*I1
+    dI1dt = sigma*E1 - (muN1+muI)*I1 - psi2*I1 - g1*I1
 
-    dI2dt = sigma*E2 - (muN2*3)*I2 - psi2*I2
+    dI2dt = sigma*E2 + g1*I1 - (muN2+muI)*I2 - psi2*I2 - g2*I2
     
-    dI3dt = sigma*E3 - (muN3*3)*I3 - psi3*I3
+    dI3dt = sigma*E3 + g2*I2 - (muN3+muI)*I3 - psi3*I3
     
-    dWdt = 0.5*lambda*I1/A + lambda*I2/A + theta*lambda*I3/A - (muW + muH)*W
+    dWdt = lambda*I1/A + theta1*lambda*I2/A + theta2*lambda*I3/A - (muW + muH)*W
     
     
     return(list(c(dS1dt, dS2dt, dS3dt, dE1dt, dE2dt, dE3dt, dI1dt, dI2dt, dI3dt, dWdt)))
   }) 
 } 
 
-# Set initial values and parameters #######
-area = 1000 #1 hectare = 10000m^2
-nstart.sn = c(S1 = 5*area, S2 = 0, S3 = 0, E1 = 0, E2 = 0, E3 = 0, I1 = 0, I2 = 0, I3 = 0, W = 10)
-t.sn = seq(0,365*300,10)
-
-par.snails=c(
-  ## Location parameters
-  A = area,          # Area of site of interest, m^2
-  H = 1000,          # Human population at site of interest
-  
-  ## Reproductive parameters
-  f = 0.26,          # Birth rate of adult snails (snails/reproductive snail/day, including survival to detection; more like a recruitment rate)
-  Kn = 50*area,      # Carrying capacity of snails (snails/m^2), from Sokolow et al. 2015
-  z = 0.5,           # Fraction of exposed snails that can reproduce, from Sokolow et al. 2015
-  
-  ## Snail mortality parameters
-  muN1 = 1/50,       # Natural mortality rate of small snails (deaths/snail/day; assume mean lifespan = 40 days)
-  muN2 = 1/75,       # Natural mortality rate of medium snails (deaths/snail/day; assume mean lifespan = 50 days)
-  muN3 = 1/100,       # Natural mortality rate of large snails (deaths/snail/day; assume mean lifespan = 60 days)
-  muI = 1/25,        # Additional mortality rate of shedding snails as a result of infection, from Sokolow et al. 2015
-  
-  ## Predation parameters
-  psi1 = 0,          # Predation rate of prawns on small snails, ignored in this model
-  psi2 = 0,          # Predation rate of prawns on medium snails, ignored in this model
-  psi3 = 0,          # Predation rate of prawns on large snails, ignored in this model
-  
-  ## Snail growth parameters
-  g1 = 1/37,         # Growth rate of small snails (size class transition rate, in terms of days to grow 4mm; adapted from McCreesh et al. 2014, assuming water temp. of 25 C)
-  g2 = 1/62,         # Growth rate of medium snails (size class transition rate, in terms of days to grow 4mm; adapted from McCreesh et al. 2014, assuming water temp. of 25 C)
-  
-  ## Infection parameters
-  beta = 4e-6,       # Human-to-snail infection probability in reference area (infected snails/miracidia/snail/day); from Sokolow et al. 2015 
-  m = 0.8,           # Miracidial shedding rate per adult female worm divided by miracidial mortality; from Sokolow et al. 2015
-  sigma = 1/30,      # Latent period for exposed snails (infectious snails/exposed snail/day); adjusted from Sokolow et al. 2015 (original value: 1/50)
-  lambda = 0.005,  # Snail-to-human infection probability scaled to 1 m^2 (composite including cercarial shedding, mortality, infection, survival to patency) from Sokolow et al. 2015; =1 infection per 127 contacts
-  theta = 2,         # Scale factor describing increase in cercarial shedding rate in larger snails; from Chu & Dawood 1970 (estimated to be between 2 and 10)
-  phi = 0.2,           # clumping parameter of the negative binomial distribution used in the mating probability function
-  
-  ## Schisto mortality parameters
-  muW = 1/(3.3*365), # Natural mortality rate of adult worms in humans, assuming average lifespan of 3.3 years, from Sokolow et al. 2015
-  muH = 1/(60*365)   # Natural mortality of humans (contributing to worm mortality), assuming average lifespan of 60 years, from Sokolow et al. 2015
-)
-
-
-
-#Run to equilibrium to eqbm values of state parameters #########
-sn.eqbm.run = as.data.frame(ode(nstart.sn,t.sn,snail_epi,par.snails))
-  sn.eqbm = sn.eqbm.run[dim(sn.eqbm.run)[1],]
+#Run to equilibrium to eqbm values of state parameters
+allvh.eqbm.run = as.data.frame(ode(nstart.sn,t.sn,snail_epi_allvh,par.snails))
+  allvh.eqbm = allvh.eqbm.run[dim(allvh.eqbm.run)[1],]
---
+++ Epi_Model/snail_epi_no_diag_plots.R
@@ -0,0 +1,82 @@
+## Epidemiological model including snail size classes; no predation
+
+## To-do list:
+  # Continue tuning infection parameters, draw estimates from literature as necessary
+  # May want to consider a few aspects of snail dynamics further, e.g.: 
+      # Should exposed snails be able to reproduce at a reduced rate?
+      # Should growth rates differ by infection status?
+      # Think a little more about diagonal transitions
+      # Reconsider area scaling
+  # Come up with R0 expression?
+
+source('Epi_Model/snail_epi_mod_no_diag.R')
+
+# Run & plot #########
+  allvh.eqbm.run$S.t = (allvh.eqbm.run$S1 + allvh.eqbm.run$S2 + allvh.eqbm.run$S3) / area        # density susceptible snails
+  allvh.eqbm.run$E.t = (allvh.eqbm.run$E1 + allvh.eqbm.run$E2 + allvh.eqbm.run$E3) / area        # density exposed snails 
+  allvh.eqbm.run$I.t = (allvh.eqbm.run$I1 + allvh.eqbm.run$I2 + allvh.eqbm.run$I3 ) / area                         # density infected snails
+  allvh.eqbm.run$N.t = (allvh.eqbm.run$S.t + allvh.eqbm.run$E.t + allvh.eqbm.run$I.t)            # density snails
+  allvh.eqbm.run$t.1 = (allvh.eqbm.run$S1 + allvh.eqbm.run$E1) / area                          # density snails of size class 1
+  allvh.eqbm.run$t.2 = (allvh.eqbm.run$S2 + allvh.eqbm.run$E2 + allvh.eqbm.run$I2) / area      # density snails of size class 2
+  allvh.eqbm.run$t.3 = (allvh.eqbm.run$S3 + allvh.eqbm.run$E3 + allvh.eqbm.run$I3) / area      # density snails of size class 3
+  # Estimated prevalence, using a negative binomial dist. with k = 0.2(fitted from EPLS data in nb_fit.R)
+  allvh.eqbm.run$prev = pnbinom(2, size = 0.2, mu = allvh.eqbm.run$W, lower.tail = FALSE)   
+
+#plot infection classes over time  
+plot(x = allvh.eqbm.run$time, y = allvh.eqbm.run$N.t, type = 'l', col = 'black', lwd=2, xlab = 'Time (days)', 
+     ylab = 'Snail density', ylim = c(0,max(allvh.eqbm.run$N.t)), 
+     main = 'Snail Infection Classes')
+  lines(allvh.eqbm.run$time, allvh.eqbm.run$I.t, col = 'red', lwd = 2)
+  lines(allvh.eqbm.run$time, allvh.eqbm.run$E.t, col = 'orange', lwd = 2)
+  lines(allvh.eqbm.run$time, allvh.eqbm.run$S.t, col = 'green', lwd = 2)
+    legend('topright', legend = c('total', 'S', 'E', 'I'), lwd = 2, 
+           col = c('black', 'green', 'orange', 'red'), cex = 0.7, bty = 'n')
+
+#plot size classes over time
+plot(x = allvh.eqbm.run$time, y = allvh.eqbm.run$N.t, type = 'l', col = 'black', lwd=2, xlab = 'Time (days)', 
+     ylab = 'Snail density', ylim = c(0,max(allvh.eqbm.run$N.t)),
+     main = 'Snail Size Classes')
+  lines(allvh.eqbm.run$time, allvh.eqbm.run$t.1, col = 'green', lwd = 2)
+  lines(allvh.eqbm.run$time, allvh.eqbm.run$t.2, col = 'blue', lwd = 2)
+  lines(allvh.eqbm.run$time, allvh.eqbm.run$t.3, col = 'red', lwd = 2)
+    legend('topright', legend = c('total', '1', '2', '3'), lwd = 2, col = c('black', 'green', 'blue', 'red'), cex = 0.7)
+  
+
+#plot worm burden over time  
+plot(x = allvh.eqbm.run$time, y = allvh.eqbm.run$W, type = 'l', col = 'red', lwd=2, xlab = 'Time (days)', 
+     ylab = 'Worm burden', ylim = c(0,max (allvh.eqbm.run$W)),
+     main = 'Worm Burden')
+
+#plot prevalence over time
+plot(x = allvh.eqbm.run$time, y = allvh.eqbm.run$prev, type = 'l', col = 'red', lwd=2, xlab = 'Time (days)', 
+     ylab = 'Prevalence', ylim = c(0,max (allvh.eqbm.run$prev)),
+     main = 'Estimated Prevalence')
+
+#plot infection dynamics stratified by size class
+opar<-par()
+par(mfrow = c(3,1), mar = c(4,3.75,1,0.4)+0.1)
+
+plot(x = allvh.eqbm.run$time, y = allvh.eqbm.run$S1/area, type = 'l', col = 'green', lwd=2, 
+     xlab = '', ylab = 'Snail density / m^2', 
+     ylim = c(0,max(allvh.eqbm.run$S1/area)),
+     main = 'Snail Infection Classes, size class 1')
+  lines(allvh.eqbm.run$time, allvh.eqbm.run$E1/area, col = 'orange', lwd = 2)
+  lines(allvh.eqbm.run$time, allvh.eqbm.run$I1/area, col = 'red', lwd = 2)
+  legend('topright', legend = c('S', 'E', 'I'), lwd = 2, col = c('green', 'orange', 'red'), cex = 0.7)
+  
+plot(x = allvh.eqbm.run$time, y = allvh.eqbm.run$S2/area, type = 'l', col = 'green', lwd=2,
+     xlab = '', ylab = 'Snail density / m^2', 
+     ylim = c(0,max(allvh.eqbm.run$S2/area)),
+     main = 'Snail Infection Classes, size class 2')
+  lines(allvh.eqbm.run$time, allvh.eqbm.run$E2/area, col = 'orange', lwd = 2)
+  lines(allvh.eqbm.run$time, allvh.eqbm.run$I2/area, col = 'red', lwd = 2)
+  
+plot(x = allvh.eqbm.run$time, y = allvh.eqbm.run$S3/area, type = 'l', col = 'green', lwd=2, 
+     xlab = 'Time (days)', ylab = 'Snail density / m^2', 
+     ylim = c(0,max(allvh.eqbm.run$S3/area)),
+     main = 'Snail Infection Classes, size class 3')
+  lines(allvh.eqbm.run$time, allvh.eqbm.run$E3/area, col = 'orange', lwd = 2)
+  lines(allvh.eqbm.run$time, allvh.eqbm.run$I3/area, col = 'red', lwd = 2)
+
+  par(opar) #Return to default plot settings
+  
