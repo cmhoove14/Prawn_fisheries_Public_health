1a185128b4c22fc103959366e617db98aa6de06b
Chris Hoover <cmhoove14@gmail.com>
2018-10-16 22:46:55 -0700

    Pull out mating function and add prevalence function and function to estimate DALYs

---
+++ R/helper_fxs.R
@@ -0,0 +1,46 @@
+#Helper functions for dynamic model
+
+fx<-function(x, 
+             mean.worm, 
+             clump){
+  alpha<-(mean.worm)/(clump+mean.worm)
+  (1-cos(x)) / ( (1+(alpha*cos(x)))^(1+clump) )
+}
+
+phi_Wk<-function(W, 
+                 phi){
+  alpha<-W/(W+phi)
+  1-( (1-alpha)^(phi+1) * (integrate(fx, 0, 2*pi, W, phi, stop.on.error = F)$value) /(2*pi)  )
+}
+
+get_prev <- function(clump, 
+                     burden){
+  pnbinom(1, size = clump, mu = burden, lower.tail = FALSE)
+}
+
+est_dalys <- function(weights_lo, # disability weight for low infection intensity 
+                      weights_hi, # disability weight for high infection intensity >=50 eggs/10mL
+                      epmL,       # estimate of eggs/10mL per mated female worm
+                      clump,      # clumping parameter of NB
+                      burden,     # mean worm burden of NB
+                      pop,        # human population
+                      daily = TRUE){  # estimating DALYs on a daily basis?
+  eggs_mL <- rnbinom(pop, size = clump, mu = burden) * phi_Wk(burden, clump) * 0.5 * epmL # estimate of egg burden converted from worm burden
+  
+  n_lo <- length(eggs_mL[eggs_mL > 0 & eggs_mL < 50])
+  n_hi <- length(eggs_mL[eggs_mL >= 50])
+  
+  if(daily == TRUE){
+    
+    dalys_lo <- n_lo * weights_lo/365
+    dalys_hi <- n_hi * weights_hi/365
+    
+  } else {
+    
+    dalys_lo <- n_lo * weights_lo
+    dalys_hi <- n_hi * weights_hi
+
+  }
+  
+  return(dalys_hi + dalys_lo)
+}
